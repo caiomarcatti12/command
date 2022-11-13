71a60b94912042b9d402318016ae7f7b200e3789
Caio Marcatti <caio.marcatti@rb.com.br>
2022-11-13 06:53:55 -0300

    fix enable command

--- src/Resolver/EnableCommandResolver.php
+++ src/Resolver/EnableCommandResolver.php
@@ -5,12 +5,17 @@ namespace CaioMarcatti12\Command\Resolver;
 use CaioMarcatti12\Command\Annotation\EnableCommand;
 use CaioMarcatti12\Core\Bean\Annotation\AnnotationResolver;
 use CaioMarcatti12\Core\Bean\Interfaces\ClassResolverInterface;
-
+use ReflectionClass;
 #[AnnotationResolver(EnableCommand::class)]
 class EnableCommandResolver implements ClassResolverInterface
 {
     public function handler(object &$instance): void
     {
-        //Not have implements
+        $reflectionClass = new ReflectionClass($instance);
+
+        $attributes = $reflectionClass->getAttributes(EnableCommand::class);
+
+        /** @var EnableCommand $attribute */
+        $attribute = ($attributes[0]->newInstance());
     }
 }
