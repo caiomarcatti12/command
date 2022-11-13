b78ef479d33a102f289c286fa35841f181aa8158
Caio Marcatti <caio.marcatti@rb.com.br>
2022-11-13 06:45:15 -0300

    fix enable command

--- src/Annotation/EnableCommand.php
+++ src/Annotation/EnableCommand.php
@@ -5,13 +5,12 @@ namespace CaioMarcatti12\Command\Annotation;
 use Attribute;
 use CaioMarcatti12\Core\Modules\Modules;
 use CaioMarcatti12\Core\Modules\ModulesEnum;
-use CaioMarcatti12\Command\Adapter\RabbitMQAdapter;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 class EnableCommand
 {
     public function __construct()
     {
-        Modules::enable(ModulesEnum::QUEUE_MANAGER);
+        Modules::enable(ModulesEnum::COMMAND);
     }
 }
