3c166f4337cb21df4b5f48efcaf892d8f6a44e24
Caio Marcatti <caio.marcatti@rb.com.br>
2022-11-13 06:40:18 -0300

    feature events

--- src/CommandServer.php
+++ src/CommandServer.php
@@ -5,12 +5,14 @@ namespace CaioMarcatti12\Command;
 use CaioMarcatti12\Cli\Interfaces\ArgvParserInterface;
 use CaioMarcatti12\Command\Objects\Routes;
 use CaioMarcatti12\Core\Factory\Annotation\Autowired;
+use CaioMarcatti12\Core\Factory\InstanceFactory;
 use CaioMarcatti12\Core\Factory\Invoke;
 use CaioMarcatti12\Core\Modules\Modules;
 use CaioMarcatti12\Core\Modules\ModulesEnum;
 use CaioMarcatti12\Core\Shared\Interfaces\ServerRunInterface;
 use CaioMarcatti12\Core\Validation\Assert;
 use CaioMarcatti12\Data\Request\Objects\Body;
+use CaioMarcatti12\Event\Interfaces\EventManagerInterface;
 use CaioMarcatti12\Webserver\Exception\RouteNotFoundException;
 
 class CommandServer implements ServerRunInterface
@@ -28,5 +30,11 @@ class CommandServer implements ServerRunInterface
 
         Body::set($this->argvParser->getAll());
         Invoke::new($route->getClass(), $route->getClassMethod());
+
+        if (Modules::isEnabled(ModulesEnum::EVENT) && $route !== null) {
+            /** @var EventManagerInterface $eventManager */
+            $eventManager = InstanceFactory::createIfNotExists(EventManagerInterface::class);
+            $eventManager->dispatch();
+        }
     }
 }
